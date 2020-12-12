78c7a7d9be5b3ce0bdaabe207a44ecb1fc957356
Your Name <you@example.com>
2020-12-12 15:04:55 +0200

    move variables to vartf

---
+++ main/terraform.tfvars
@@ -0,0 +1,5 @@
+vpc_cidr = "10.0.0.0/16"
+subnet_a_cidr = "10.0.1.0/24"
+subnet_b_cidr = "10.0.2.0/24"
+container_name = "eitan-nodejs-slim"
+
--- main/variables.tf
+++ main/variables.tf
@@ -1,20 +1,16 @@
 variable vpc_cidr {
-    description = "vpc cidr"
-    default = "10.0.0.0/16"
+    description = "vpc cidr"    
 }
 
 variable subnet_a_cidr {
-    description = "subnet a cidr"
-    default = "10.0.1.0/24"
+    description = "subnet a cidr"    
 }
 
 variable subnet_b_cidr {
     description = "subnet b cidr"
-    default = "10.0.2.0/24"
 }
 
 variable "container_name" {
   type        = string
-  default     = "eitan-nodejs-slim"
 }
 
